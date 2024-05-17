fa90e65391b70303257e40a9e7d4a7cf428d0b66
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-05-17 21:38:04 +0200

    feat(games/flappy-bird): css reset

--- apps/pixi-vanilla/source/main.ts
+++ apps/pixi-vanilla/source/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+import { Application } from "pixi.js";
+import "./styles/reset.css";
+
+(async () => {
+  const app = new Application();
+
+  await app.init({ background: "#1099bb", resizeTo: window });
+  document.body.appendChild(app.canvas);
+})();
