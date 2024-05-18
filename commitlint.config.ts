61ef7da024d6394d2e96e6ffdae545aedd413ff0
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-05-18 19:27:19 +0200

    refactor(root): changed scope rules for commitlint. reduced max line width to 80 for biome

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from "@commitlint/types";
 const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["root", "pixijs"]]
+    "scope-enum": [2, "always", ["root", "pixi-pixijs"]]
   }
 };
 
