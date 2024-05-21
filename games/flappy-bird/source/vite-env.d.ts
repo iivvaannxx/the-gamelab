ce6c1e8a127e0090d81ebdfa96015dc35b1f8352
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-05-21 19:49:50 +0200

    refactor(games/flappy-bird): simplified pixijs types augmentation

--- games/flappy-bird/source/vite-env.d.ts
+++ games/flappy-bird/source/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
-import { Application } from "pixi.js";
+import type { Application } from "pixi.js";
 
 declare module "pixi.js" {
-  declare interface Application {
+  namespace Application {
     // For comfortable access to the application instance.
     // The correct alternative to this singleton could probably be to use DI.
     // But its too much for a simple game.
@@ -11,11 +11,6 @@ declare module "pixi.js" {
     // It's only used to access the dimensions of the screen
     // without the need to pass around the app instance.
 
-    /** The current instance of the application. */
-    instance: Application;
-  }
-
-  namespace Application {
     /** The current instance of the application. */
     const instance: Application;
   }
