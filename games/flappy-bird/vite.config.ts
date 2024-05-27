4fa9a585f8f6c2435ed875e98aaf3ca1c459760c
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-05-27 22:53:22 +0200

    feat(games/flappy-bird): configured pwa

--- games/flappy-bird/vite.config.ts
+++ games/flappy-bird/vite.config.ts
@@ -1,6 +1,40 @@
 import { defineConfig } from "vite";
+
+import { VitePWA } from "vite-plugin-pwa";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
+  plugins: [
+    tsConfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Flappy Bird",
+        short_name: "Flappy Bird",
+        description:
+          "🎮 A clone of the popular Flappy Bird game. Made with Typescript and Pixi.js",
+
+        theme_color: "#121212",
+        background_color: "#121212",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,webp,xml,ico,ogg}"],
+      },
+    }),
+  ],
 });
